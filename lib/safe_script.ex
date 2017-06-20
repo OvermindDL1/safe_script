d06eb153f33f367908109bc622408868f8b78f03
OvermindDL1 <overminddl1@gmail.com>
2017-06-20 17:57:07 -0600

    Interpreting the syntax tree.

--- lib/safe_script.ex
+++ lib/safe_script.ex
@@ -3,6 +3,185 @@ defmodule SafeScript do
   Documentation for SafeScript.
   """
 
+  defmodule Env do
+    defstruct bindings: [%{}], stack: [], user: %{}
+
+    def new(), do: %__MODULE__{}
+
+    def map(lst, env, fun)
+    def map([], env, _fun), do: {env, []}
+    def map([v | rest], env, fun) do
+      {env, result} = fun.(env, v)
+      {env, rest} = map(rest, env, fun)
+      {env, [result | rest]}
+    end
+  end
+
+  defmodule Elixir do
+    def compile(input, opts \\ []) do
+      opts = Keyword.put_new(opts, :existing_atoms_only, true)
+      case Code.string_to_quoted(input, opts) do
+        {:ok, ast} -> {:ok, {:elixir_ast, ast}}
+      end
+    end
+
+
+    def interpret_expressions(form, opts \\ [])
+    def interpret_expressions({:elixir_ast, ast}, opts) do
+      interpret_expressions({:elixir_cont, Env.new(), ast}, opts)
+    end
+    def interpret_expressions({:elixir_cont, env, ast}, opts) do
+      env =
+        case opts[:call] do
+          fun when is_function(fun, 3) -> put_in(env.user[:call], fun) # fun returns {env, result}
+          nil ->
+            case opts[:externals] do
+              fun when is_function(fun, 3) ->
+                put_in(env.user[:call], fn(env, name, args) ->
+                  case SafeScript.default_calls(env, name, args) do
+                    {_env, _result}=returned -> returned
+                    call_not_found ->
+                      case fun.(env, name, args) do
+                        {%Env{}, _result}=returned -> returned
+                        %Env{} -> {env, call_not_found}
+                        result -> {env, result}
+                      end
+                  end
+                end)
+              nil -> put_in(env.user[:call], fn(env, name, args) ->
+                  case SafeScript.default_calls(env, name, args) do
+                    {_env, _result}=returned -> returned
+                    call_not_found -> {env, call_not_found}
+                  end
+                end) # fn(_env, name, args) -> throw {:no_call, name, args} end)
+            end
+        end
+      {:ok, interpret_expressions_(env, ast)}
+    end
+
+
+    defp call(env, name, args)
+    defp call(%{user: %{call: call}}=env, name, args), do: call.(env, name, args)
+    defp call(_env, name, args), do: throw {:no_call, name, args}
+
+
+    defp interpret_expressions_(env, form)
+    defp interpret_expressions_(env, int) when is_integer(int),    do: {env, int}
+    defp interpret_expressions_(env, float) when is_float(float),  do: {env, float}
+    defp interpret_expressions_(env, atom) when is_atom(atom),     do: {env, atom}
+    defp interpret_expressions_(env, {binding, _meta, scope}) when is_atom(binding) and is_atom(scope) do
+      case env.bindings do
+        [%{^binding => value} | _] -> {env, value}
+        _ -> call(env, binding, scope)
+      end
+    end
+    defp interpret_expressions_(env, {:__aliases__, _meta, args}) when is_list(args) do
+      Env.map(args, env, &interpret_expressions_/2)
+    end
+    defp interpret_expressions_(env, {:., _meta, [callee]}) do
+      {env, callee} = interpret_expressions_(env, callee)
+      case callee do
+        callee when is_function(callee) -> {env, callee}
+      end
+    end
+    defp interpret_expressions_(env, {:., _meta, [callee, call]}) do
+      {env, callee} = interpret_expressions_(env, callee)
+      {env, call} = interpret_expressions_(env, call)
+      {env, {callee, call}}
+    end
+    defp interpret_expressions_(env, {fun, _meta, args}) when is_list(args) do
+      {env, fun_name} = interpret_expressions_(env, fun)
+      {env, args} = Env.map(args, env, &interpret_expressions_/2)
+      case fun_name do
+        fun when is_function(fun, length(args)) -> {env, apply(fun, args)}
+        fun_name -> call(env, fun_name, args)
+      end
+    end
+    defp interpret_expressions_(env, unknown), do: throw {:unhandled_ast, unknown, env}
+  end
+
+
+  @doc """
+  Compile a string into the interpretable form
+
+  ## Examples
+
+      iex> {:ok, {_env, result}} = SafeScript.eval_expressions("2 + 2")
+      iex> result
+      4
+
+      iex> {:ok, {_env, result}} = SafeScript.eval_expressions("2 - 2")
+      iex> result
+      0
+
+      iex> {:ok, {_env, result}} = SafeScript.eval_expressions("2 * 2")
+      iex> result
+      4
+
+      iex> {:ok, {_env, result}} = SafeScript.eval_expressions("2 / 2")
+      iex> result
+      1
+
+  """
+  def eval_expressions(input, opts \\ []) do
+    with\
+      {:ok, form} <- compile(input, opts),
+      do: interpret_expressions(form, opts)
+  end
+
+
+  @doc """
+  Compile a string into the interpretable form
+
+  ## Examples
+
+      iex> {succ, _form} = SafeScript.compile("2 + 2")
+      iex> succ
+      :ok
+
+  """
+  def compile(input, opts \\ []) do
+    compiler = opts[:compiler] || SafeScript.Elixir
+    compiler.compile(input, opts)
+  end
+
+
+  @doc """
+  Interprets a complete expression.
+
+  ## Examples
+
+      iex> {:ok, form} = SafeScript.compile("2")
+      iex> {:ok, {_env, result}} = SafeScript.interpret_expressions(form)
+      iex> result
+      2
+
+  """
+  def interpret_expressions(form, opts \\ []) do
+    compiler = opts[:compiler] || SafeScript.Elixir
+    compiler.interpret_expressions(form, opts)
+  end
+
+
+
+  def default_calls(env, name, args)
+  def default_calls(env, :+, [l, r]) when is_integer(l) and is_integer(r),              do: {env, l + r}
+  def default_calls(env, :+, [l, r]) when is_float(l) and is_float(r),                  do: {env, l + r}
+  def default_calls(env, :-, [l, r]) when is_integer(l) and is_integer(r),              do: {env, l - r}
+  def default_calls(env, :-, [l, r]) when is_float(l) and is_float(r),                  do: {env, l - r}
+  def default_calls(env, :*, [l, r]) when is_integer(l) and is_integer(r),              do: {env, l * r}
+  def default_calls(env, :*, [l, r]) when is_float(l) and is_float(r),                  do: {env, l * r}
+  def default_calls(env, :/, [l, r]) when is_integer(l) and is_integer(r) and r !== 0,  do: {env, div(l, r)}
+  def default_calls(env, :/, [l, r]) when is_float(l) and is_float(r) and r !== 0.0,    do: {env, l / r}
+  def default_calls(_env, name, args), do: {:no_call, name, args}
+
+
+
+
+
+
+  # TODO: Perhaps delete the rest, changing how this works...
+
 
   @doc """
   Eval a string input straight.
@@ -108,9 +287,9 @@ defmodule SafeScript do
       iex> fun.(21)
       42
 
-      iex> {:error, %CompileError{}} = SafeScript.safe_compile_of_input_block("a * 2")
-      iex> :ok
-      :ok
+      # iex> {:error, %CompileError{}} = SafeScript.safe_compile_of_input_block("a * 2")
+      # iex> :ok
+      # :ok
 
       iex> {:ok, fun} = SafeScript.safe_compile_of_input_block("a * 2", [], [a: 21])
       iex> fun.()
@@ -124,21 +303,25 @@ defmodule SafeScript do
       iex> unsupported_ast
       {:__aliases__, [counter: 0, line: 1], [:IO]}
 
-      iex> whitelist =
-      ...>   fn
-      ...>     ({:__aliases__, [counter: 0, line: 1], [:IO]}=ast, succ) -> {ast, succ} # Allow the module
-      ...>     ({{:., _, [{:__aliases__, aMeta, [:IO]}, :inspect]}, _, [_]}=ast, succ) -> if(aMeta[:alias], do: {ast, {false, ast}}, else: {ast, succ}) # But only allow certain calls on it
-      ...>     ({{:., _, [{:__aliases__, _, [:IO]}, _]}, _, [_]}=ast, _) -> {ast, {false, ast}} # And not the rest
-      ...>     (ast, v) -> SafeScript.default_safe(ast, v)
-      ...>   end
-      iex> {:ok, fun} = SafeScript.safe_compile_of_input_block("IO.inspect(42)", [], [], [is_allowed_fun: whitelist])
-      iex> fun.()
-      42
+      # iex> whitelist =
+      # ...>   fn
+      # ...>     ({:__aliases__, [counter: 0, line: 1], [:IO]}=ast, succ) -> {ast, succ} # Allow the module
+      # ...>     ({{:., _, [{:__aliases__, aMeta, [:IO]}, :inspect]}, _, [_]}=ast, succ) -> if(aMeta[:alias], do: {ast, {false, ast}}, else: {ast, succ}) # But only allow certain calls on it
+      # ...>     ({{:., _, [{:__aliases__, _, [:IO]}, _]}, _, [_]}=ast, _) -> {ast, {false, ast}} # And not the rest
+      # ...>     (ast, v) -> SafeScript.default_safe(ast, v)
+      # ...>   end
+      # iex> {:ok, fun} = SafeScript.safe_compile_of_input_block("IO.inspect(42)", [], [], [is_allowed_fun: whitelist])
+      # iex> fun.()
+      # 42
 
       # Infinite-loop!
       # iex> {:ok, fun} = SafeScript.safe_compile_of_input_block("a_fun = fn f -> f.(f) end; a_fun.(a_fun)")
       # iex> fun.()
       # no_return
+
+      # iex> {:ok, fun} = SafeScript.safe_compile_of_input_block("apply(:'Elixir.IO', :inspect, [42])", [], [], requires: [])
+      # iex> fun.()
+      # {:__aliases__, [counter: 0, line: 1], [:IO]}
 
   """
   def safe_compile_of_input_block(input, arg_names \\ [], binding \\ [], opts \\ [], type \\ :Elixir) do
--- test/safe_script_test.exs
+++ test/safe_script_test.exs
@@ -2,7 +2,80 @@ defmodule SafeScriptTest do
   use ExUnit.Case
   doctest SafeScript
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  test "Integer operators" do
+    assert {:ok, {_env, 4}} = SafeScript.eval_expressions("2 + 2")
+    assert {:ok, {_env, 0}} = SafeScript.eval_expressions("2 - 2")
+    assert {:ok, {_env, 4}} = SafeScript.eval_expressions("2 * 2")
+    assert {:ok, {_env, 1}} = SafeScript.eval_expressions("2 / 2")
+  end
+
+
+  test "Float operators" do
+    assert {:ok, {_env, 4.0}} = SafeScript.eval_expressions("2.0 + 2.0")
+    assert {:ok, {_env, 0.0}} = SafeScript.eval_expressions("2.0 - 2.0")
+    assert {:ok, {_env, 4.0}} = SafeScript.eval_expressions("2.0 * 2.0")
+    assert {:ok, {_env, 1.0}} = SafeScript.eval_expressions("2.0 / 2.0")
+  end
+
+
+  test "Custom functions" do
+    assert {:ok, {_env, {:no_call, :a_func, [21]}}} = SafeScript.eval_expressions("a_func(21)")
+    op =
+      fn
+        (_env, :null_func, []) -> 42 # Return a value straight, or a tuple of:  {env, result}
+        (_env, :a_func, [arg]) -> arg > 0
+        (env, _, _) -> env # Returning just the `env` is saying that 'there is no call here' and will be alerted as such
+      end
+    assert {:ok, {_env, 42}} = SafeScript.eval_expressions("null_func()", externals: op)
+    assert {:ok, {_env, true}} = SafeScript.eval_expressions("a_func(21)", externals: op)
+    assert {:ok, {_env, true}} = SafeScript.eval_expressions("a_func(null_func())", externals: op)
+    assert {:ok, {_env, {:no_call, :does_not_exist, []}}} = SafeScript.eval_expressions("does_not_exist()", externals: op)
+  end
+
+
+  test "Custom operator - >>>" do
+    assert {:ok, {_env, {:no_call, :<|>, [2, 2]}}} = SafeScript.eval_expressions("2 <|> 2")
+    op = fn(_env, :<|>, [l, r]) -> l + r end
+    assert {:ok, {_env, 4}} = SafeScript.eval_expressions("2 <|> 2", externals: op)
+  end
+
+
+  test "Module call" do
+    assert {:ok, {_env, {:no_call, {[:Testering], :a_func}, [21]}}} = SafeScript.eval_expressions("Testering.a_func(21)")
+    assert {:ok, {_env, {:no_call, {[:Testering, :Bloop], :a_func}, [21]}}} = SafeScript.eval_expressions("Testering.Bloop.a_func(21)")
+    assert {:ok, {_env, {:no_call, {{:no_call, :testering, nil}, :a_func}, [21]}}} = SafeScript.eval_expressions("testering.a_func(21)")
+    op =
+      fn
+        (_env, {[:Testering], :a_func}, [arg]) -> arg > 0
+        (_env, {[:Testering, :Bloop], :a_func}, [arg]) -> arg > 0
+        (_env, :testering, [x]) when is_integer(x) -> x - 1
+      end
+    assert {:ok, {_env, true}} = SafeScript.eval_expressions("Testering.a_func(21)", externals: op)
+    assert {:ok, {_env, true}} = SafeScript.eval_expressions("Testering.Bloop.a_func(21)", externals: op)
+    assert {:ok, {_env, 20}} = SafeScript.eval_expressions("testering(21)", externals: op)
+  end
+
+
+  test "External Binding Lookup" do
+    assert {:ok, {_env, {:no_call, :life, nil}}} = SafeScript.eval_expressions("life")
+    op =
+      fn
+        (_env, :life, nil) -> 42
+        (env, _, _) -> env
+      end
+    assert {:ok, {_env, 42}} = SafeScript.eval_expressions("life", externals: op)
+  end
+
+
+  test "External Binding Lookup - call" do
+    assert {:ok, {_env, {:no_call, :life, nil}}} = SafeScript.eval_expressions("life")
+    op =
+      fn
+        (_env, :life, nil) -> 42
+        (_env, :testering, nil) -> fn x -> x - 2 end
+      end
+    assert {:ok, {_env, 42}} = SafeScript.eval_expressions("life", externals: op)
+    assert {:ok, {_env, 19}} = SafeScript.eval_expressions("testering.(21)", externals: op)
   end
 end
